fdcc3ebbb0f588af5193cf6b2860982d391eaeb7
Otitochukwu Okoye <okoyeotito3@gmail.com>
2026-05-01 07:35:55 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function off(){
+    document.getElementById("On").style.display = "none";
+    document.getElementById("Off").style.display = "block";}
+    
+function on(){
+    document.getElementById("Off").style.display = "none";
+    document.getElementById("On").style.display = "block";}
